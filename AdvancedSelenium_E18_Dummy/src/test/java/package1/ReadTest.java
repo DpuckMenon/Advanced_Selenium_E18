c3284156c8f8a2a133197c6cbe0ba3e43956bc2d
Nishanth <Nishanth@DESKTOP-NSGQES8>
2025-04-06 18:23:08 +0530

    Commit and push - Dummy Project for Jenkins

---
+++ AdvancedSelenium_E18_Dummy/src/test/java/package1/ReadTest.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class ReadTest {
+	
+	@Test
+	public void WriteTest()
+	{
+		System.out.println("Write test executed Successfully");
+	}
+
+}
---
+++ AdvancedSelenium_E18_Dummy/src/test/java/package1/WriteTest.java
@@ -0,0 +1,13 @@
+package package1;
+
+import org.testng.annotations.Test;
+
+public class WriteTest {
+	
+	@Test
+	public void ReadTest()
+	{
+		System.out.println("Read test executed Successfully");
+	}
+
+}
